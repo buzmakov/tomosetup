7a59e10bd3f444b7a3ae2946f4ab8bb5c3c82b11
Alexey Buzmakov <buzmakov@gmail.com>
2015-05-05 12:25:35 +0300

    Add shuter module

--- shutter/shutter.py
+++ shutter/shutter.py
@@ -1,28 +1,34 @@
 import serial
 
+#TODO: check errors
 
-def open_shutter(tty_name, rele_number):
-    with serial.Serial(tty_name) as serial_port:
-        serial_port.write("$KE,REL,{},1\r\n".format(rele_number))
+# def open_shutter(tty_name, rele_number):
+#     with serial.Serial(tty_name) as serial_port:
+#         serial_port.write("$KE,REL,{},1\r\n".format(rele_number))
 
 
-def close_shutter(tty_name, rele_number):
-    with serial.Serial(tty_name) as serial_port:
-        serial_port.write("$KE,REL,{},0\r\n".format(rele_number))
+# def close_shutter(tty_name, rele_number):
+#     with serial.Serial(tty_name) as serial_port:
+#         serial_port.write("$KE,REL,{},0\r\n".format(rele_number))
 
 
 class Shutter(object):
 
     """Shutter for x-ray source """
 
-    def __init__(self, tty_name):
+    def __init__(self, tty_name, rele_number):
         """
         Init shutter object
 
         :param tty_name: COM port name to connect
+        :param rele_number: number of rele 1-4
         """
         super(Shutter, self).__init__()
+        
+        #TODO: check input parameters
+
         self.tty_name = tty_name
+        self.rele_number = rele_number
         self.check_module()
 
     def check_module(self):
@@ -30,25 +36,47 @@ class Shutter(object):
         Is controller alive?
         """
         with serial.Serial(self.tty_name) as serial_port:
-            if serial_port.readline() != '#OK\r\n':
-                raise RuntimeError("XRayShutter_ConnectionFailed")
-
-    def open_shutter(self, rele_number):
+            serial_port.write("$KE\r\n")
+            if not '#OK' in serial_port.readline(): 
+                raise RuntimeError('Shutter not operated')
+        
+    def open(self):
         """
-        Open rele
+        Open shutter
+        """
+        with serial.Serial(self.tty_name) as serial_port:
+            serial_port.write("$KE,REL,{},1\r\n".format(self.rele_number))
+            if not '#REL,OK' in serial_port.readline(): 
+                raise RuntimeError('Shutter not opened')
 
-        :param rele_number: number of rele 1-4
+    def close(self):
+        """
+        Close shutter      
 
         """
         with serial.Serial(self.tty_name) as serial_port:
-            serial_port.write("$KE,REL,{},1\r\n".format(rele_number))
+            serial_port.write("$KE,REL,{},0\r\n".format(self.rele_number))
+            if not '#REL,OK' in serial_port.readline(): 
+                raise RuntimeError('Shutter not closed')
 
-    def close_shutter(self, rele_number):
+
+    def is_open(self):
         """
-        Close rele
-
-        :param rele_number: number of rele 1-4
-
+        Check is shutter in open state
         """
         with serial.Serial(self.tty_name) as serial_port:
-            serial_port.write("$KE,REL,{},1\r\n".format(rele_number))
+            serial_port.write("$KE,RDR,{}\r\n".format(self.rele_number))
+            response = serial_port.readline()
+            if "#RDR,{},".format(self.rele_number) in response:
+                state = int(response.split(',')[-1])
+            else:
+                raise ValueError('Bad response')
+
+            if state == 1:
+                return True
+            elif state == 0:
+                return False
+            else:
+                raise ValueError('Bad response')
+            # if not '#REL,OK' in serial_port.readline(): 
+            #     raise RuntimeError('Shutter not closed')
--- shutter/test_shutter.py
+++ shutter/test_shutter.py
@@ -1,7 +1,11 @@
-from shutter import open_shutter, close_shutter
+from shutter import Shutter
 
 from time import sleep
 if __name__ == "__main__":
-    open_shutter('COM4', 4)
+    shutter = Shutter('COM4', 4)
+    print shutter.is_open()
+    shutter.open()
+    print shutter.is_open()
     sleep(0.5)
-    close_shutter('COM4',4)
+    shutter.close()
+    print shutter.is_open()
